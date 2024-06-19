b07009d17eb30049f0b31ae60fa12ccb6e3309cf
dd_hye <ddy.hye@gmail.com>
2024-06-19 17:23:06 +0900

    0619 (PM) 이도혜 - 출퇴근 기록 진행 중

--- src/main/java/ko/gagu/issue/main/dao/MainDAO.java
+++ src/main/java/ko/gagu/issue/main/dao/MainDAO.java
@@ -2,7 +2,14 @@ package ko.gagu.issue.main.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import ko.gagu.issue.employee.dto.Attendance_history_tbDTO;
+import ko.gagu.issue.employee.dto.EmployeeDTO;
+
 @Mapper
 public interface MainDAO {
+
+	EmployeeDTO getEmpData(String empID);
+
+	Attendance_history_tbDTO getTodayCheck(int idx_employee);
 
 }
