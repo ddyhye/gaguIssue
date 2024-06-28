8527ff3f5a974bd43a8196bf62fb2e3c39f0d8d1
구일승 <bun0531@gmail.com>
2024-06-28 17:01:00 +0900

    0628 PM - 메일 작성 진행중

--- src/main/java/ko/gagu/issue/controller/MailController.java
+++ src/main/java/ko/gagu/issue/controller/MailController.java
@@ -3,11 +3,36 @@ package ko.gagu.issue.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ko.gagu.issue.service.MailService;
 
 @Controller
 public class MailController {
 	
 	Logger logger = LoggerFactory.getLogger(getClass());
+	private final MailService mailService;
 	
+	public MailController(MailService mailService) {
+		this.mailService=mailService;
+	}
 
+	@GetMapping(value="/common/mailList.go")
+	public String mailList(){
+		logger.info("mailList in");
+		return "common/mail";
+	}
+	
+	@GetMapping(value="/common/mailWrite.go")
+	public String mailWrite() {
+		logger.info("메일작성 페이지 진입");
+		return "common/mailWrite";
+	}
+	
+	@PostMapping(value="/common/mailWrite.do")
+	publci Stirng send
+	
+	
+	
 }
