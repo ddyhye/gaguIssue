224494588c2f40c5a410e8273955c74d0981099d
구일승 <bun0531@gmail.com>
2024-07-13 15:32:58 +0900

    0713 오후 메일 리스트, 상세보기, 페이징, 다운로드 완료 -> 다운로드는 확인 필요함.

--- src/main/java/ko/gagu/issue/controller/MailController.java
+++ src/main/java/ko/gagu/issue/controller/MailController.java
@@ -11,12 +11,17 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import ko.gagu.issue.dto.MailDTO;
 import ko.gagu.issue.service.MailService;
@@ -26,6 +31,7 @@ public class MailController {
 	
 	Logger logger = LoggerFactory.getLogger(getClass());
 	private final MailService mailService;
+	
 	
 	public MailController(MailService mailService) {
 		this.mailService=mailService;
@@ -48,7 +54,7 @@ public class MailController {
 	public String sendMail(MailDTO mailDto, @RequestParam("files") MultipartFile[] files,HttpSession session) throws MessagingException {
 		logger.info("mailDto : {}",mailDto.toString());
 		logger.info("제목 : {}",mailDto.getTitle());
-		logger.info("받는 사람 문자 : {}",Arrays.stream(mailDto.getAddress())
+		logger.info("받는 사람 문자열 : {}",Arrays.stream(mailDto.getAddress())
                 .collect(Collectors.joining(",")));
 		int idx_employee = (int) session.getAttribute("idxEmployee");
 		logger.info("idx_employee : {}",idx_employee);
@@ -65,22 +71,51 @@ public class MailController {
 	}
 	
 	@PostMapping(value="/common/sendMailList.ajax")
-	public Map<String, Object>sendingMailList(String page, String cnt,int idx_sending_email){
+	@ResponseBody
+	public Map<String, Object>sendingMailList(String page, String cnt){
 		logger.info("페이지 당 보여줄 갯수 : "+cnt);
 		logger.info("요청 페이지 : " +page);
 		
 		int currentPage = Integer.parseInt(page);
 		int pagePerCnt = Integer.parseInt(cnt);
-		
-		Map<String, Object>map = mailService.sendingMailList(idx_sending_email,currentPage,pagePerCnt);
+		logger.info("sendingmaillist 진입 전");
+		logger.info("currentPage : {}",currentPage);
+		logger.info("pagePerCnt : {}",pagePerCnt);
+		Map<String, Object>map = mailService.sendingMailList(currentPage,pagePerCnt);
 		logger.info("map : {}",map);
 		
 		return map;
 	}
 	
+//	@GetMapping(value="/common/sendMailDetail.go")
+//	public String sendMailDetail(Integer idx_sending_email,Model model) {
+//		logger.info("idx_sending_email : {}",idx_sending_email);
+//		MailDTO mailDto = mailService.sendMailDetail(idx_sending_email);
+//		logger.info("mailDto : {}",mailDto);
+//		model.addAttribute("mailDto",mailDto);
+//		return "common/mailSendingDetail";
+//	}
 	
-
+	@GetMapping(value="/common/sendMailDetail.go")
+	public ModelAndView sendMailDetail(Integer idx_sending_email) {
+		ModelAndView mav= new ModelAndView();
+		
+		Map<String, Object>map=mailService.getSendMailDetail(idx_sending_email);
+		logger.info("map : {}",map);
+		if (map!=null) {
+			mav.addObject("map",map);
+			mav.setViewName("common/mailSendingDetail");
+		}else {
+			mav.setViewName("errorPage");
+		}
+		return mav;
+	}
 	
+	@GetMapping(value="/download/{fileName}")
+	public ResponseEntity<Resource>mailDownload(@PathVariable String fileName){
+		logger.info("fileName : {}",fileName);
+		return mailService.mailDownload(fileName);
+	}
 	
 	
 }
